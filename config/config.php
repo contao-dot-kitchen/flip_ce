81ef8ec8d43ad4361a1b55b765e49cb8a6b68525
root <root@static.148-251-70-116.clients.your-server.de>
2016-04-18 08:32:05 +0200

    added copyright informations and comments

--- config/config.php
+++ config/config.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * Content elements
+ */
 array_insert($GLOBALS['TL_CTE'], 3, array
 (
 	'flip' => array
@@ -13,6 +16,9 @@ array_insert($GLOBALS['TL_CTE'], 3, array
 	)
 ));
 
+/**
+ * jQuery
+ */
 $GLOBALS['TL_JQUERY']['flipJS'] = '<script src="system/modules/flip_ce/assets/js/jquery.flip.min.js"></script>
 <script>
 $(function($) {
@@ -21,9 +27,21 @@ $(function($) {
 </script>
 ';
 
+/**
+ * Content wrappers
+ **/
 $GLOBALS['TL_WRAPPERS']['start'][] = 'flipStart';
 $GLOBALS['TL_WRAPPERS']['stop'][]  = 'flipStop';
 $GLOBALS['TL_WRAPPERS']['start'][] = 'flipFrontStart';
 $GLOBALS['TL_WRAPPERS']['stop'][]  = 'flipFrontStop';
 $GLOBALS['TL_WRAPPERS']['start'][] = 'flipBackStart';
 $GLOBALS['TL_WRAPPERS']['stop'][]  = 'flipBackStop';
+
+/**
+ * Copyright information
+ */
+$GLOBALS['TL_HEAD']['PIXELSPREADDE'] = '<!--
+    This Contao OpenSource CMS uses modules from pixelSpread.de
+    Copyright (c)2012 - 2014 by Sascha Brandhoff :: Extensions of pixelSpread.de are copyright of their respective owners
+    Visit our website at http://www.pixelSpread.de for more information
+//-->';
